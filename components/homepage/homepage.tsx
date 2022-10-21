c3016fb06c72f4b2e86595ad8d746b1a5c545e6a
jason1642 <jason1642@gmail.com>
2022-10-21 12:31:50 -0400

    styling

--- components/homepage/homepage.tsx
+++ components/homepage/homepage.tsx
@@ -9,8 +9,10 @@ interface IHomeProps {
 const Home: React.FunctionComponent<IHomeProps> = (props) => {
   return (
     <div className={homeStyles.container}>
-      This is the home page
+      
       <LargeSection />
+
+
     </div>
   );
 };
--- components/homepage/largeSection.tsx
+++ components/homepage/largeSection.tsx
@@ -1,13 +1,28 @@
 import * as React from 'react';
 import largeSectionStyle from './largeSection.module.css'
+import Image from 'next/image'
+import IpadCoverImage from '../../public/images/ipad-home-image.jpeg'
+
 
 interface ILargeSectionProps {
 }
 
 const LargeSection: React.FunctionComponent<ILargeSectionProps> = (props) => {
   return (
-    <div className={largeSectionStyle.container}>
+    <div className={largeSectionStyle.container}> 
+      
+      <div className={largeSectionStyle.imageContainer} >
+        <img
+          // objectFit='contain'
+          // layout='fill'
+          className={largeSectionStyle.coverImage}
+          src={'https://images.unsplash.com/photo-1608386390658-86cf60bcf61a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80'}
+        />
+      </div>
 
+      <div className={largeSectionStyle.infoWrapper}>
+       New iPad 10th Generation
+      </div>
     </div>
   );
 };
