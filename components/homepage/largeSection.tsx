5ff2c77a7e2069a6dd44b8a1e9f00dd80e6efadd
jason1642 <jason1642@gmail.com>
2022-10-21 20:37:47 -0400

    homepage styles

--- components/homepage/largeSection.tsx
+++ components/homepage/largeSection.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import largeSectionStyle from './largeSection.module.css'
 import Image from 'next/image'
 import iPhone14Pro from '../../public/images/iphone14pro.png'
-
+import Link from 'next/link'
 
 interface ILargeSectionProps {
 }
@@ -22,9 +22,25 @@ const LargeSection: React.FunctionComponent<ILargeSectionProps> = (props) => {
         <div className={largeSectionStyle.label} >
         iPhone 14 Pro
         </div>
+
+
         <div style={{ fontSize: '1.6em', fontWeight: '300'}} className={largeSectionStyle.label} >
         Pro. Beyond.
         </div>
+
+        <div className={largeSectionStyle.buttonTray}>
+          <Link href={''}>
+            <div className={largeSectionStyle.button}>Learn more {">"}</div>
+          </Link>
+          
+          <Link href={''}>
+            <div className={largeSectionStyle.button}>
+              Buy {">"}
+            </div>
+        </Link>
+
+        </div>
+
       </div>
     </div>
   );
